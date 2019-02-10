8ffc0bae7397e93a073a291580d207cc7e37f387
shazam.yy <shazam.yy@alibabapictures.com>
2019-02-10 10:53:59 +0800

    docker mysql init

---
+++ docker/mysql/mysql-docker-init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+docker_mysql_image_name = "mysql"
+docker_mysql_image_tag = "latest"
+mysql_port = "3306"
+docker_mysql_port = "3306"
+mysql_data_path = "/Users/user/yangyong/dev/mysql/data"
+mysql_root_password = "yangyong"
+docker_mysql_name = "young-mysql"
+mysql_server_encode = "utf8mb4"
+mysql_collation_server_encode = "utf8mb4_unicode_ci"
+docker_mysql_command = "--name $docker_mysql_name -p $mysql_port:$docker_mysql_port -v $mysql_data_path:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysql_root_password -d $docker_mysql_image_name:$docker_mysql_image_tag --character-set-server=$mysql_server_encode --collation-server=$mysql_collation_server_encode"
+docker run "$docker_mysql_command"
---
+++ docker/mysql/mysql-docker-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker_mysql_name = "young-mysql"
+docker start "$docker_mysql_name"
